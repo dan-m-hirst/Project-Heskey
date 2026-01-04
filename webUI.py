be4d6fdc41a400be9fb22edbd23da2011412dd26
dan-m-hirst <hirst.daniel@outlooko.com>
2026-01-04 00:29:35 +0000

    Added session-based title re-intialisation. Added checkbox list for players, instead of feeding from CSV

--- webUI.py
+++ webUI.py
@@ -18,17 +18,41 @@ def get_teams(all_player_stats_path, players_playing_path):
     random.shuffle(team_b)
     return {"Team A": team_a, "Team B": team_b}
 
+def get_sponsor():
+    if "sponsor" not in st.session_state:
+        sponsor_list = pd.read_csv("Simulator Files/sponsors.csv", header = None)[0]
+        sponsor = random.choice(sponsor_list)
+        return(sponsor)
+    return(st.session_state["sponsor"])
+
+while False:
+    #have basically dummied this as don't want to focus on procedural dialogue
+    #before draw table is done
+    footy_unc_list = pd.read_csv("Simulator Files/uncs.csv", header = None)["Name"]
+
+
+# Get fairest teams and generate randomised sponsor for the draw
 randomised_teams = get_teams("Player Files/Player Stats.csv", "Player Files/Who is playing.csv")
 
-sponsor_list = pd.read_csv("Simulator Files/sponsors.csv", header = None)[0]
-sponsor = random.choice(sponsor_list)
-footy_unc_list = pd.read_csv("Simulator Files/uncs.csv", header = None)["Name"]
 
+#######BEGIN WEBPAGE######
+sponsor = get_sponsor()
+st.session_state["sponsor"] = sponsor
 st.title(f"Patented Mike Dixon 4most Draw Simulator - Brought to you by {sponsor}")
 st.title(100*"-")
 st.write("Hello World!")
 
 st.set_page_config(layout="wide")
+
+active_players = []
+no_players = len(active_players)
+for player in sorted(pd.read_csv("Player Files/Player Stats.csv")["Player"].tolist()):
+    if st.checkbox(f"{player}"):
+        active_players.append(player)
+        no_players = len(active_players)
+
+st.write(f"{no_players} players set to play.")
+
 
 if st.button("Begin the festivities"):
     #we want to run all of our procedural dialogue and draw in here
